2592a86aa666e54d85a422bc7a6971f714387655
intZaibi <shahzaibalisomroo@gmail.com>
2025-03-04 06:46:15 +0500

    add messages api route

--- app/api/messages/route.js
+++ app/api/messages/route.js
@@ -1,14 +1,32 @@
+import db from '@/utils/db';
 import { NextResponse } from "next/server";
 
 export async function POST(req){
   const {formData} = await req.json();
   console.log(formData);
+  try{
+
+    await db.query('INSERT INTO messages VALUES ?, ?, ?, ?', [
+      formData.name, formData.email, formData.whatsapp, formData.message
+    ]);
+  } catch(err) {
+
+    console.log('Error inserting messages: ', err)
+    return NextResponse.json({Error: 'Something went wrong!'}, {status: 500})
+  }
 
   return NextResponse.json({message: "Message recieved!"}, {status: 200})
 }
 
 
-export async function GET(req){
-
-  return NextResponse.json({message: "Message recieved!"}, {status: 200})
+export async function GET(){
+  try {
+    
+    const messages = await db.query('SELECT * FROM messages');
+    return NextResponse.json({ messages }, {status: 200})
+  } catch (error) {
+    
+    console.log('Error retrieving messages: ', error);
+    return NextResponse.json({Error: "Something went wrong!"}, {status: 500})
+  }
 }
